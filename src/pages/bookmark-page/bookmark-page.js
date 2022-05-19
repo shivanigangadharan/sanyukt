bee68b2c81c12d4f390a14f340bbc53311fbdb2d
shivanigangadharan <shivani.gangadharan@gmail.com>
2022-05-19 12:21:20 +0530

    Bookmarks page and explore page

--- src/pages/bookmark-page/bookmark-page.js
+++ src/pages/bookmark-page/bookmark-page.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import Sidebar from '../../components/sidebar/sidebar';
+import CreatePost from '../../components/create-post/create-post';
+import Post from '../../components/post/post';
+import FollowThem from '../../components/follow-them/follow-them';
+import '../../styles.css';
 
 export default function BookmarkPage() {
     return (
-        <div>
-            <h1> BookmarkPage </h1>
+        <div className="homepage-container">
+            <Sidebar />
+            <div className="homepage-posts">
+                <h3> Your bookmarks </h3>
+                <Post />
+                <Post />
+                <Post />
+            </div>
+            <FollowThem />
         </div>
     )
 }
--- src/pages/explore-page/explore-page.js
+++ src/pages/explore-page/explore-page.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import Sidebar from '../../components/sidebar/sidebar';
+import CreatePost from '../../components/create-post/create-post';
+import Post from '../../components/post/post';
+import FollowThem from '../../components/follow-them/follow-them';
+import '../../styles.css';
+import './explore-page.css';
 
 export default function ExplorePage() {
     return (
-        <div>
-            <h1> ExplorePage </h1>
+        <div className="homepage-container">
+            <Sidebar />
+            <div className="homepage-posts">
+                <h3> Explore </h3>
+                <div className="explore-categories">
+                    <button> For you </button>
+                    <button> Trending </button>
+                    <button> Technology </button>
+                    <button> Self care </button>
+                    <button> World </button>
+                </div>
+                <Post />
+                <Post />
+                <Post />
+            </div>
+            <FollowThem />
         </div>
     )
 }
