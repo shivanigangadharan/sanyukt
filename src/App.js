cf02925e49e1e68bba05809a8d01390633b29a2f
shivanigangadharan <shivani.gangadharan@gmail.com>
2022-06-10 13:03:27 +0530

    css fixes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import AppRoutes from "./AppRoutes";
 import { BrowserRouter } from "react-router-dom";
+import Header from "./components/header/header";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Header />
         <AppRoutes />
       </BrowserRouter>
     </div>
---
+++ src/components/header/header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from '../../assets/sanyuktLogo.png';
+import { Link } from 'react-router-dom';
+import './header.css';
+
+export default function Header() {
+    return (
+        <div className="header-container">
+            <Link to="/homepage" className="header-content">
+                <img src={logo} alt="logo" className="brand-logo" />
+                <span className="brand-name"> Sanyukt </span>
+            </Link>
+        </div>
+    )
+}
